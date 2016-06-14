e29b9b3e4693fffc34cbf94c14bbcef5537c79f2
ozthekoder <oozdemir2704@gmail.com>
2016-06-14 19:57:05 -0400

    List view finished

--- src/components/ListView/ListView.js
+++ src/components/ListView/ListView.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Button from 'react-toolbox/lib/button';
+import { List, ListItem, ListSubHeader, ListDivider, ListCheckbox } from 'react-toolbox/lib/list';
+import Immutable from 'immutable'
 import classes from './ListView.scss'
 
 class ListView extends React.Component {
@@ -7,15 +8,75 @@ class ListView extends React.Component {
     super(props);
   }
 
-  flatten(tree) {
+  flatten(path=[], tree) {
+    let before = tree
+    .get('$before')
+    .map((op, index) =>{
+      return this.flatten([...path, '$before', index], op)
+    })
 
+    if(before.size > 0) {
+      before = before.reduce((prev, next) => prev.concat(next))
+    }
+    let after = tree
+    .get('$after')
+    .map((op, index) =>{
+      return this.flatten([...path, '$after', index], op)
+    })
+    if(after.size > 0) {
+      after = after.reduce((prev, next) => prev.concat(next))
+    }
+
+    let beforeEach = tree
+    .get('$beforeEach')
+    .map((op, index) =>{
+      return this.flatten([...path, '$beforeEach', index], op)
+    })
+    if(beforeEach.size > 0) {
+      beforeEach = beforeEach.reduce((prev, next) => prev.concat(next))
+    }
+
+    let afterEach = tree
+    .get('$afterEach')
+    .map((op, index) =>{
+      return this.flatten([...path, '$afterEach', index], op)
+    })
+    if(afterEach.size > 0) {
+      afterEach = afterEach.reduce((prev, next) => prev.concat(next))
+    }
+
+    let ops;
+    if(tree.has('$ops')) {
+      ops = tree
+      .get('$ops', Immutable.List())
+      .map((op, index) =>{
+        return this.flatten([...path, '$ops', index], op)
+      })
+      .map((op) => beforeEach.concat(op).concat(afterEach))
+
+      if(ops.size > 0) {
+        ops = ops.reduce((prev, next) => prev.concat(next))
+      }
+    } else {
+      ops = beforeEach
+      .concat(Immutable.List([tree.set('$path', path)]))
+      .concat(afterEach)
+    }
+
+    return ops.size ? before.concat(ops).concat(after) : Immutable.List();
   }
 
   render(){
     return (
       <div className='box'>
         <div className="column">
-
+        <List selectable ripple>
+        {
+         this
+         .flatten([], this.props.blueprint)
+         .map((op) => <span><ListItem onClick={this.props.setContext.bind(null, op.get('$path'))} caption={op.get('$name')} legend={`${op.get('$plugin')}.${op.get('$op')}`} /><ListDivider /></span>)
+        }
+        </List>
         </div>
       </div>
     )
