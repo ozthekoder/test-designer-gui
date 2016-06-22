36081620025e6a2aeecd261b4bfb3ab508ad0e8f
Oz <osman.ozdemir@adp.com>
2016-06-21 22:52:17 -0400

    bash script for bin improved

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/zsh
 
 PRT=3000
+CWD="${pwd}"
+
 for i in "$@"
 do
   case $i in
@@ -11,4 +13,27 @@ do
   esac
 done
 
-NODE_ENV=production PORT=PRT npm run deploy:prod && node_modules/.bin/http-server -p "${PRT}" -o
+SOURCE="${BASH_SOURCE[0]}"
+
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd "${DIR}/../"
+
+if grep -Fxq "localhost:${PRT}" public/index.html
+then
+  echo build found! starting server..;
+else
+  echo build not found! yarak;
+
+  #npm run deploy:prod && node_modules/.bin/http-server -p "${PRT}" -o
+  node_modules/.bin/http-server -p "${PRT}" -o
+
+fi
+
+cd $CWD
