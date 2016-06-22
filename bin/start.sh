20a2ec1bb678a792d7c43b3c4a779b2999cc1510
Oz <osman.ozdemir@adp.com>
2016-06-21 23:45:19 -0400

    bash script fix

--- bin/start.sh
+++ bin/start.sh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 
 PRT=3000
-CWD="${pwd}"
 
 for i in "$@"
 do
@@ -13,27 +12,5 @@ do
   esac
 done
 
-SOURCE="${BASH_SOURCE[0]}"
-
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-
-cd "${DIR}/../"
-
-if grep -Fxq "localhost:${PRT}" public/index.html
-then
-  echo build found! starting server..;
-else
-  echo build not found! yarak;
-
-  #npm run deploy:prod && node_modules/.bin/http-server -p "${PRT}" -o
-  node_modules/.bin/http-server -p "${PRT}" -o
-
-fi
-
-cd $CWD
+DROOT=$(npm root)/test-designer-gui/public
+node_modules/.bin/http-server $DROOT -p $PRT -d False -o
